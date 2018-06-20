f7818d83f11a1546fa8dff62523d9f375a30c2d2
Sugir <ragusugir@gmail.com>
2018-06-20 19:30:25 +0530

    added queries

--- IRproject/spiders/theHubSpider.py
+++ IRproject/spiders/theHubSpider.py
@@ -10,13 +10,15 @@ class theHubSpider(scrapy.Spider):
         externalLinks = response.css("div.threadinfo div.inner h3.threadtitle a::attr(href)")
         for link in externalLinks:
             yield response.follow(link, self.parse_page)
-        print ("==============================")
-
     
     def parse_page(self, response):
-        print ("++++++++++++++++++++")
         title = response.xpath("/html/body/div[2]/div[5]/ol/li[1]/div[2]/div[2]/div[1]/h2/text()")
-        auther = response.xpath("/html/body/div[2]/div[5]/ol/li[1]/div[2]/div[1]/div[2]/div/a/strong/text()")
-        print (title)
-        print (auther)
-        print ("-----------------------")
+        author = response.xpath("/html/body/div[2]/div[5]/ol/li[1]/div[2]/div[1]/div[2]/div/a/strong/text()")
+        startedDate = response.xpath("/html/body/div[2]/div[5]/ol/li[1]/div[1]/span[1]/span/text()")
+        startedTime = response.xpath("/html/body/div[2]/div[5]/ol/li[1]/div[1]/span[1]/span/span/text()")
+        description = response.xpath("/html/body/div[2]/div[5]/ol/li[1]/div[2]/div[2]/div[1]/div/div/blockquote/text()")
+        category = response.xpath("/html/body/div[2]/div[1]/ul/li[4]/a/span/text()")
+        print (title.extract_first(), author.extract(), startedDate.extract(),startedTime.extract(), description.extract(), category.extract())
+
+
+        
